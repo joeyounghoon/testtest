1e70b649a001d209313b656d7d64e3eccaea2e6c
joeyounghoon <yhjoe36511@gmail.com>
2024-06-18 23:52:48 +0900

    Update 선픽.py

--- 선픽.py
+++ 선픽.py
@@ -435,12 +435,19 @@ def call_example(query):
     }
     return examples.get(query, {"team": [], "counter": []})
 
-html = ""
+html_ad = ""
 for item in champions_ad:
     name=item["name"]
     src = item["image_url"]
-    html += f"<a href='#' id='{name}'><img src='{src}'></a>"
+    html_ad += f"<a href='#' id='{name}'><img src='{src}'></a>"
+
+html_sup = ""
+for item in champions_sup:
+    name=item["name"]
+    src = item["image_url"]
+    html_sup += f"<a href='#' id='{name}'><img src='{src}'></a>"
     
+
 
 
 # 중앙 정렬을 위한 컨테이너
@@ -448,7 +455,7 @@ col1, col2 = st.columns(2)
 clicked=None
 with col1:
     with st.container():
-        clicked = click_detector(html)
+        clicked = click_detector(html_ad)
         #cols = st.columns(6)
         #for i in range(len(champions_ad)):
         #    with cols[i % 6]:
@@ -481,9 +488,38 @@ with col2:
 
 st.divider()
 
-with st.container():
-    cols = st.columns(6)
-    for i in range(len(champions_sup)):
-        with cols[i % 6]:
-            champion_sup = champions_sup[i]
-            st.image(champion_sup["image_url"], caption=champion_sup["name"])
+col1, col2 = st.columns(2)
+clicked=None
+with col1:
+    with st.container():
+        clicked = click_detector(html_sup)
+        #cols = st.columns(6)
+        #for i in range(len(champions_ad)):
+        #    with cols[i % 6]:
+        #        champion_ad = champions_ad[i]
+        #        st.image(champion_ad["image_url"], caption=champion_ad["name"])
+with col2:
+    with st.container():
+        #placeholder = st.empty()
+        st.write(clicked)
+        # call openai
+        result = call_example(clicked)
+        #st.write(result)
+        # call openai
+        st.subheader("Team")
+        for item in result['team']:
+            for i in champions_ad:
+                if i["name"] == item:
+                    st.image(i['image_url'])
+            for i in champions_sup:
+                if i["name"] == item:
+                    st.image(i['image_url'])
+        st.subheader("Counter")
+        for item in result['counter']:
+            for i in champions_ad:
+                if i["name"] == item:
+                    st.image(i['image_url'])
+            for i in champions_sup:
+                if i["name"] == item:
+                    st.image(i['image_url'])
+
