71b952385335c4441d383b9c2929ab586df50c24
joeyounghoon <yhjoe36511@gmail.com>
2024-06-18 15:42:06 +0900

    Update 선픽.py

--- 선픽.py
+++ 선픽.py
@@ -232,9 +232,19 @@ with col2:
     with st.container():
         #placeholder = st.empty()
         st.write(clicked)
+        # call openai
         result = call_example(clicked)
         st.write(result)
-        # call openai
+        st.subheader("Team")
+        for item in result['team']:
+            for i in champions_ad:
+                if i["name"] == item:
+                    st.image(i['image_url'])
+        st.subheader("Counter")
+        for item in result['counter']:
+            for i in champions_ad:
+                if i["name"] == item:
+                    st.image(i['image_url'])
 
 st.divider()
 
